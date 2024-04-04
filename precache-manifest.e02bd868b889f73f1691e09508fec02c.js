2397cdb7f685568df9cb6634aeb3bbcf4e4e2f02
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2024-04-04 13:56:21 -0600

    Updates

---
+++ precache-manifest.e02bd868b889f73f1691e09508fec02c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531bb5a420876d3b81b412ab083b9575",
+    "url": "/%7BJaredFGPortfolio%7D/index.html"
+  },
+  {
+    "revision": "14a65e9d8e66da26f7eb",
+    "url": "/%7BJaredFGPortfolio%7D/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2bc84631e5d0beb4f4f1",
+    "url": "/%7BJaredFGPortfolio%7D/static/css/main.0faf4ff0.chunk.css"
+  },
+  {
+    "revision": "14a65e9d8e66da26f7eb",
+    "url": "/%7BJaredFGPortfolio%7D/static/js/2.607a1682.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/%7BJaredFGPortfolio%7D/static/js/2.607a1682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc84631e5d0beb4f4f1",
+    "url": "/%7BJaredFGPortfolio%7D/static/js/main.52169792.chunk.js"
+  },
+  {
+    "revision": "b68b806274edb48bf48b",
+    "url": "/%7BJaredFGPortfolio%7D/static/js/runtime-main.3775e823.js"
+  },
+  {
+    "revision": "dd60c5ecb88c97557cf9daf6344021eb",
+    "url": "/%7BJaredFGPortfolio%7D/static/media/BannerPrincipal.dd60c5ec.png"
+  }
+]);
